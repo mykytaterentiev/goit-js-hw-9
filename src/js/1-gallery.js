b3676ac38fdc8bce718716271214b33af43dcdea
mykytaterentiev <mykytaterentievua@gmail.com>
2024-01-21 14:59:28 +0100

    fix21

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,60 +1,94 @@
-const form = document.querySelector('.feedback-form');
-const localStorageKey = 'feedback-form-state';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-function saveFormDataToLocalStorage(formData) {
-  localStorage.setItem(localStorageKey, JSON.stringify(formData));
-}
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
 
-function restoreDataFromLocalStorage() {
-  return JSON.parse(localStorage.getItem(localStorageKey)) || {};
-}
+const galleryContainer = document.querySelector('.gallery');
 
-const savedFormData = restoreDataFromLocalStorage();
+images.forEach(({ preview, original, description }) => {
+  const listItem = document.createElement('li');
+  const link = document.createElement('a');
+  const img = document.createElement('img');
 
-const formattedFormData = {
-  email: (savedFormData.email || '').trim(),
-  message: (savedFormData.message || '').trim(),
-};
+  listItem.classList.add('gallery-item');
 
-form.elements.email.value = formattedFormData.email;
-form.elements.message.value = formattedFormData.message;
+  link.classList.add('gallery-link');
+  link.href = original;
 
-const handleInput = event => {
-  formattedFormData[event.target.name] = event.target.value.trim();
+  img.classList.add('gallery-image');
+  img.src = preview;
+  img.alt = description;
 
-  saveFormDataToLocalStorage(formattedFormData);
-};
+  link.appendChild(img);
+  listItem.appendChild(link);
+  galleryContainer.appendChild(listItem);
+});
 
-const handleSubmit = event => {
-  event.preventDefault();
-  let isEmptyField = false;
-
-  for (const key in formattedFormData) {
-    if (
-      formattedFormData.hasOwnProperty(key) &&
-      formattedFormData[key] === ''
-    ) {
-      isEmptyField = true;
-      break;
-    }
-  }
-
-  if (isEmptyField) {
-    alert('Please fill in all fields');
-    return;
-  }
-
-  console.log({
-    email: form.elements.email.value,
-    message: form.elements.message.value,
-  });
-
-  localStorage.removeItem(localStorageKey);
-  form.reset();
-
-  formattedFormData.email = '';
-  formattedFormData.message = '';
-};
-
-form.addEventListener('input', handleInput);
-form.addEventListener('submit', handleSubmit);
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250
+});
